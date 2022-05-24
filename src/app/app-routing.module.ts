b968274fea340509ecd4314dc9030a6393f1f47b
PoschnerMarie <marie.poschner@edu.siemens-energy.com>
2022-05-24 12:15:27 +0200

    added routing for component tabellenbeschriftung

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TabellenbeschreibungComponent } from './component/tabellenbeschreibung/tabellenbeschreibung.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tabellenbeschriftung', component: TabellenbeschreibungComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
