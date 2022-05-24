edad88538965cee7a2b562b91478cbb6ea7eafe3
PoschnerMarie <marie.poschner@edu.siemens-energy.com>
2022-05-24 12:07:49 +0200

    component tabellenbeschriftung

---
+++ src/app/component/tabellenbeschreibung/tabellenbeschreibung.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabellenbeschreibung',
+  templateUrl: './tabellenbeschreibung.component.html',
+  styleUrls: ['./tabellenbeschreibung.component.css']
+})
+export class TabellenbeschreibungComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
