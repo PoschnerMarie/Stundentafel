b6834649e9aef7c347268d95ba3a23004f29251b
mirella9952 <mirella9952@gmail.com>
2022-05-25 13:22:15 +0200

    TAbelleNichtGut

--- src/app/component/tafel/tafel.component.ts
+++ src/app/component/tafel/tafel.component.ts
@@ -1,13 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Stundentafel {
+  montag: string;
+  dienstag: string;
+  mittwoch: string;
+  donnerstag: string;
+  freitag: string;
+}
+
+const ELEMENT_DATA: Stundentafel[] = [
+  {montag: 'Montag', dienstag: 'Dienstag', mittwoch: 'Mittwoch', donnerstag: 'Donnerstag', freitag: 'freitag'}
+];
+
 @Component({
   selector: 'app-tafel',
   templateUrl: './tafel.component.html',
   styleUrls: ['./tafel.component.css']
+  
 })
 export class TafelComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag','Freitag'];
+  dataSource = ELEMENT_DATA;
+  
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
   }
